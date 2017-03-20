46017ac12450d5f6bdd9474aaf1b6c3d17cc0fbf
Carlos Filoteo <cfiloteo@ancestry.com>
2017-03-20 09:01:43 -0600

    Some attempt at testing the modal component.

--- src/app/modal/modal.component.spec.ts
+++ src/app/modal/modal.component.spec.ts
@@ -22,4 +22,22 @@ describe('ModalComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should open and emit an open event', () => {
+    expect(component.open).not.toBeUndefined();
+    component.opened.subscribe($e => {
+      expect($e).toBeNull();
+      done();
+    })
+    component.open();
+  })
+
+  it('should close and emit a close event', () => {
+    expect(component.close).not.toBeUndefined();
+    component.closed.subscribe($e => {
+      expect($e).toBeNull();
+      done();
+    })
+    component.close();
+  })
 });
